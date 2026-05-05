ea3d68ba0de07a44f7058c539f634930a503536d
Jack Gribble <jgribble2@outlook.com>
2026-05-05 23:34:08 +0000

    intermediate orders

---
+++ models/int_orders.sql
@@ -0,0 +1,31 @@
+WITH source AS (
+    SELECT *
+    FROM {{ source('raw', 'orders') }}
+),
+clean_orders AS (
+    SELECT
+        ordr_id AS order_id,
+        cust_id AS customer_id,
+        produkt_id AS product_id,
+        dt_id AS date_id,
+        statsus AS order_status,
+        qty AS quantity,
+        rev AS revenue
+    FROM source
+    WHERE
+        order_id IS NOT NULL
+        AND customer_id IS NOT NULL
+        AND product_id IS NOT NULL
+        AND date_id IS NOT NULL
+        AND order_status = 'COMPLETE'
+        AND quantity IS NOT NULL
+        AND revenue IS NOT NULL
+)        
+SELECT
+    customer_id,
+    COUNT(*) AS customer_orders,
+    SUM(revenue) AS customer_revenue
+FROM
+    clean_orders
+GROUP BY
+    customer_id
