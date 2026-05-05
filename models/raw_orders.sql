1bb77c198f20552657fed6eccec86a5b0535d731
Jack Gribble <jgribble2@outlook.com>
2026-05-05 23:03:07 +0000

    returned column headers to original dirty names

--- models/raw_orders.sql
+++ models/raw_orders.sql
@@ -4,11 +4,11 @@ WITH source AS (
 )
 
 SELECT
-    ordr_id AS order_id,
-    cust_id AS customer_id,
-    produkt_id AS product_id,
-    dt_id AS date_id,
-    statsus AS order_status,
-    qty AS quantity,
-    rev AS revenue
+    ordr_id,
+    cust_id,
+    produkt_id,
+    dt_id,
+    statsus,
+    qty,
+    rev
 FROM source
